eb28891d6637e78105e47e30da30b2bb0274f891
Anthony Dcosta <adcosta2000@gmail.com>
2022-08-19 23:20:20 -0400

    modified savedBooks.js file for GraphQL

--- client/src/pages/SavedBooks.js
+++ client/src/pages/SavedBooks.js
@@ -1,40 +1,59 @@
 import React, { useState, useEffect } from 'react';
 import { Jumbotron, Container, CardColumns, Card, Button } from 'react-bootstrap';
+import {useQuery, useMutation} from '@apollo/client';//import useQuery
+import {GET_ME} from '../utils/queries';// import GET_ME query
+import {REMOVE_BOOK} from '../utils/mutations';
 
-import { getMe, deleteBook } from '../utils/API';
+// import { getMe, deleteBook } from '../utils/API';
 import Auth from '../utils/auth';
 import { removeBookId } from '../utils/localStorage';
 
 const SavedBooks = () => {
-  const [userData, setUserData] = useState({});
+  const[removeBook, {error}] = useMutation(REMOVE_BOOK);
 
-  // use this to determine if `useEffect()` hook needs to run again
-  const userDataLength = Object.keys(userData).length;
+  const {loading, data} = useQuery(GET_ME);
+  const userData = data?.me || {};
 
-  useEffect(() => {
-    const getUserData = async () => {
-      try {
-        const token = Auth.loggedIn() ? Auth.getToken() : null;
+  if(loading) {
+    return <div>Loading....</div>
+  }
 
-        if (!token) {
-          return false;
-        }
+  if(!user?.username) {
+    return (
+      <h4>
+        You need to be logged in to seet his page. Use the navigation links about to sign up or log in!
+      </h4>
+    );
+  }
+  // const [userData, setUserData] = useState({});
 
-        const response = await getMe(token);
+  // // use this to determine if `useEffect()` hook needs to run again
+  // const userDataLength = Object.keys(userData).length;
 
-        if (!response.ok) {
-          throw new Error('something went wrong!');
-        }
+  // useEffect(() => {
+  //   const getUserData = async () => {
+  //     try {
+  //       const token = Auth.loggedIn() ? Auth.getToken() : null;
 
-        const user = await response.json();
-        setUserData(user);
-      } catch (err) {
-        console.error(err);
-      }
-    };
+  //       if (!token) {
+  //         return false;
+  //       }
 
-    getUserData();
-  }, [userDataLength]);
+  //       const response = await getMe(token);
+
+  //       if (!response.ok) {
+  //         throw new Error('something went wrong!');
+  //       }
+
+  //       const user = await response.json();
+  //       setUserData(user);
+  //     } catch (err) {
+  //       console.error(err);
+  //     }
+  //   };
+
+  //   getUserData();
+  // }, [userDataLength]);
 
   // create function that accepts the book's mongo _id value as param and deletes the book from the database
   const handleDeleteBook = async (bookId) => {
@@ -45,13 +64,19 @@ const SavedBooks = () => {
     }
 
     try {
-      const response = await deleteBook(bookId, token);
+      // const response = await deleteBook(bookId, token);
 
-      if (!response.ok) {
-        throw new Error('something went wrong!');
-      }
+      // if (!response.ok) {
+      //   throw new Error('something went wrong!');
+      // }
 
-      const updatedUser = await response.json();
+      const {data} = await removeBook({
+        variables: {bookId:bookId}
+      });
+
+      const updatedUser = data.user
+
+      // const updatedUser = await response.json();
       setUserData(updatedUser);
       // upon success, remove book's id from localStorage
       removeBookId(bookId);
