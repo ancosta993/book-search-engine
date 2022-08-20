fea787aebdcd162aaca541fb9a98b5d995d0288d
Anthony Dcosta <adcosta2000@gmail.com>
2022-08-19 22:27:12 -0400

    front end (react) queries and mutations added for execution (untested)

---
+++ client/src/utils/mutations.js
@@ -0,0 +1,53 @@
+import {gql} from '@apollo/client';
+
+export const LOGIN_USER = gql`
+   mutation loginUser ($email: String!, $password: String!) {
+      loginUser(email: $email, password: $password) {
+         token
+         user {
+            _id
+            username
+         }
+      }
+   }
+`;
+
+export const ADD_USER = gql`
+   mutation addUser ($username: String!, $email: String!, $password: String!){
+      addUser(username:$username, email: $email, password: $password) {
+         token
+         user {
+            _id
+            username
+         }
+      }
+   }
+`;
+
+export const SAVE_BOOK = gql`
+   mutation saveBook ($bookId: String!, $authors:[String!]!, $description:String!, $title:String!, $image:String!, $link: String!) {
+      saveBook(bookId:$bookId, authors:$authors, description:$description, title:$title, image:$image, link:$link) {
+         token
+         user {
+            _id
+            username
+            bookCount
+            savedBooks
+         }
+      }
+   }
+`;
+
+export const REMOVE_BOOK = gql`
+   mutation removeBook ($bookId: String!) {
+      removeBook(bookId: $bookId) {
+         token
+         user{
+            _id
+            username
+            bookCount
+            savedBooks
+         }
+      }
+   }
+`;
